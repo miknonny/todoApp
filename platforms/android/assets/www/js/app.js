0ddbed6723419559561a9ef94c66879227456bc5
nonnycode <miknonny@gmail.com>
2014-07-09 09:31:38 +0100

    added some changed to the App

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -3,5 +3,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('todo', ['ionic'])
 
+var todoApp = angular.module('todo', ['ionic'])
+
+
+
+todoApp.controller('TodoCtrl', ['$scope', function ($scope) {
+  $scope.tasks = [
+    {title: 'Collect coins'},
+    {title: 'Eat mushrooms'},
+    {title: 'Get high enough to grab the flag'},
+    {title: 'Find the princess'}
+  ];
+}]);
--- www/js/app.js
+++ www/js/app.js
@@ -3,5 +3,49 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('todo', ['ionic'])
+
+var todoApp = angular.module('todo', ['ionic'])
+
+
+
+todoApp.controller('TodoCtrl', ['$scope', 'ionicModal' function ($scope, $ionicModal) {
+  
+  //Some test data
+  $scope.tasks = [
+    {title: 'Collect coins'},
+    {title: 'Eat mushrooms'},
+    {title: 'Get high enough to grab the flag'},
+    {title: 'Find the princess'}
+  ];
+  //Create the task and load the modal.
+  
+  //fromTemplate Method takes three arguments
+  //the function loads the modal into the scope
+  //we then set the modal scope the root $scope
+  //and the animation for the modal to slide up.
+  $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+    $scope.taskModal = modal,
+  }, {
+    scope: $scope
+    animation: 'slide-in-up'
+  });
+  //called when the form is submitted
+  $scope.createTask = function (task) {
+    $scope.task.push({
+      title: task.title
+    });
+     $scope.taskModal.hide();
+     task.title = "";
+    };
+
+  //Open the new task modal.
+  $scope.newTask = function () {
+    $scope.taskModal.show();
+  }
+  
+  //Close the new task modal
+  $scope.closeNewTask = function () {
+    $scope.taskModal.close();
+  }
+}]);
 
